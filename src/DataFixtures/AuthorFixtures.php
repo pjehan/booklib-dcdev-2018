fe5b9c79f8ef55737070be2b83d4108e41974a46
pjehan <pierre.jehan@gmail.com>
2018-11-21 19:55:16 +0100

    Ajout de fixtures utilisant Faker

--- src/DataFixtures/AuthorFixtures.php
+++ src/DataFixtures/AuthorFixtures.php
@@ -6,6 +6,7 @@ use App\Entity\Author;
 use App\Entity\Book;
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Common\Persistence\ObjectManager;
+use Faker\Factory;
 
 class AuthorFixtures extends Fixture
 {
@@ -18,12 +19,23 @@ class AuthorFixtures extends Fixture
             ["Emile", "Zola"],
         ];
 
-        foreach ($authors as $author) {
+        foreach ($authors as $key => $author) {
             $aut = new Author();
             $aut->setFirstname($author[0]);
             $aut->setLastname($author[1]);
             $manager->persist($aut);
-            $this->setReference('author-' . strtolower($author[1]), $aut);
+            $this->setReference('author-' . ($key + 1), $aut);
+        }
+
+        $faker = Factory::create('fr_FR');
+
+        for($i = 5; $i < 100; $i++) {
+            $faker->seed($i);
+            $author = new Author();
+            $author->setFirstname($faker->firstName);
+            $author->setLastname($faker->lastName);
+            $manager->persist($author);
+            $this->setReference('author-' . $i, $author);
         }
 
         $manager->flush();
---
+++ src/DataFixtures/BorrowFixtures.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace App\DataFixtures;
+
+
+use App\Entity\Borrow;
+use Doctrine\Bundle\FixturesBundle\Fixture;
+use Doctrine\Common\DataFixtures\DependentFixtureInterface;
+use Doctrine\Common\Persistence\ObjectManager;
+use Faker\Factory;
+
+class BorrowFixtures extends Fixture implements DependentFixtureInterface
+{
+
+    public function load(ObjectManager $manager)
+    {
+        $faker = Factory::create('fr_FR');
+
+        for ($i = 0; $i < 20; $i++) {
+            $borrow = new Borrow();
+            $borrow->setBook($this->getReference('book-' . $faker->numberBetween(1, 20)));
+            $datestart = $faker->dateTime;
+            $borrow->setDateStart($datestart);
+            $borrow->setBoxFrom($this->getReference('box-' . $faker->numberBetween(1, 99)));
+            if ($faker->boolean) {
+                $borrow->setBoxTo($this->getReference('box-' . $faker->numberBetween(1, 99)));
+                $borrow->setDateEnd($datestart->add(new \DateInterval('P' . $faker->numberBetween(1, 30) . 'D')));
+            }
+            $borrow->setUser($this->getReference('user-' . $faker->numberBetween(1, 20)));
+            $manager->persist($borrow);
+        }
+
+        $manager->flush();
+    }
+
+    public function getDependencies()
+    {
+        return [UserFixtures::class, BoxFixtures::class, BookFixtures::class];
+    }
+
+}
